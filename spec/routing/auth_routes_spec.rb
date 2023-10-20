87028955b97025d6546674b590cb6b9f2910ee95
Mohamed Nabil <mohamednabil00000@gmail.com>
2023-10-20 12:01:41 +0300

    Added routing specs (#18)

---
+++ spec/routing/auth_routes_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe AuthenticationController, type: :routing do
+  describe 'routing' do
+    it 'routes to #login' do
+      expect(post: '/auth/login').to route_to('authentication#login')
+    end
+  end
+end
