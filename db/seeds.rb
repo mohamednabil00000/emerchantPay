8575160e553e2652ac6698bc44bb1036da851961
Mohamed Nabil <mohamednabil00000@gmail.com>
2023-10-22 02:10:31 +0300

    Addind user seeds (#27)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,3 +7,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Admin.create!(name: 'admin', email: 'admin@gmail.com', password: '12345678', password_confirmation: '12345678')
+Merchant.create!(name: 'merchant', email: 'merchant@gmail.com', password: '12345678', password_confirmation: '12345678')
