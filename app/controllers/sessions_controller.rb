39f5f09ee7116493a51493ef5eb4ba39077fbb69
Mohamed Nabil <mohamednabil00000@gmail.com>
2023-10-22 22:44:25 +0300

    Fixed auth token in session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,24 +3,16 @@
 class SessionsController < ApplicationController
   skip_before_action :authorize, only: %i[new create]
 
-  def new
-    @form = LoginForm.new
-  end
+  def new; end
 
   def create
-    @form = LoginForm.new(login_form_params)
-    if @form.submit
-      session[:auth_token] = auth_token
+    result = auth_service.login(email: params[:email], password: params[:password], user_type: params[:user_type])
+    if result.successful?
+      session[:auth_token] = result.attributes[:token]
       # TO-DO redirect to page of transactions
     else
       flash[:notice] = I18n.t('errors.messages.wrong_email_or_password')
       redirect_to action: 'new'
     end
-  end
-
-  private
-
-  def login_form_params
-    params.require(:login_form).permit(:email, :password, :user_type)
   end
 end
--- spec/requests/sessions_controller_spec.rb
+++ spec/requests/sessions_controller_spec.rb
@@ -7,13 +7,13 @@ RSpec.describe SessionsController, type: %i[api controller] do
     let!(:user) { create(:admin, email: 'test@test.com', password: '12345678', name: 'test') }
     let(:user_type) { 'admin' }
     it 'when login is successful' do
-      post_request(params: { login_form: { email: 'test@test.com', password: '12345678', user_type: } }, path: :create)
+      post_request(params: { email: 'test@test.com', password: '12345678', user_type: }, path: :create)
       expect(session).to have_key(:auth_token)
       # TO-DO test the redirect
     end
 
     it 'when login is unsuccessful' do
-      post_request(params: { login_form: { email: 'test@test.com', password: '123456789', user_type: } }, path: :create)
+      post_request(params: { email: 'test@test.com', password: '123456789', user_type: }, path: :create)
       expect(session[:auth_token]).to be_nil
       expect(flash[:notice]).to eq 'Wrong email or Password!'
       expect(response).to redirect_to('/')
