1fb46b3880d3e64fdb81d49784cfc1cce361b7e0
Mohamed Nabil <mohamednabil00000@gmail.com>
2023-10-23 17:45:38 +0300

    Added development environment for cron jobs in docker file

--- docker/prepare-cron-jobs.sh
+++ docker/prepare-cron-jobs.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
 # update cron jobs
-bundle exec whenever --update-crontab
+bundle exec whenever --update-crontab --set 'environment=development'
 
 echo "cron jobs updated successfully!"
