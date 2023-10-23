93bff9c88a2982ac2ebd5392686b7eaf2f5b308c
Mohamed Nabil <mohamednabil00000@gmail.com>
2023-10-23 17:47:31 +0300

    Added default env for cron jobs

--- config/schedule.rb
+++ config/schedule.rb
@@ -4,7 +4,7 @@ ENV.each_key do |key|
   env key.to_sym, ENV.fetch(key, nil)
 end
 
-set :environment, ENV.fetch('RAILS_ENV', nil)
+set :environment, ENV.fetch('RAILS_ENV', 'development')
 
 every 1.hour do
   rake 'transactions:delete'
