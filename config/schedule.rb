a49c255e41a56544dbbdae726b88c9cea6e87ef7
Mohamed Nabil <mohamednabil00000@gmail.com>
2023-10-23 17:51:09 +0300

    Task27: Added development environment for cron jobs in docker file (#36)
    * Added development environment for cron jobs in docker file
    
    * Added default env for cron jobs
    
    * removed unnecessary lines

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
-ENV.each_key do |key|
-  env key.to_sym, ENV.fetch(key, nil)
-end
-
-set :environment, ENV.fetch('RAILS_ENV', nil)
+set :environment, ENV.fetch('RAILS_ENV', 'development')
 
 every 1.hour do
   rake 'transactions:delete'
